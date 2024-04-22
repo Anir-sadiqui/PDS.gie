8a29acd0341ed1b5acf43966848b38926b5e3ace
anir-sadiqui <Anirsadiqui2@gmail.com>
2024-04-22 20:15:23 +0100

    PDS Release 1 (CRM)

--- gieFrontend/src/main/java/org/giefront/DTO/Personne.java
+++ gieFrontend/src/main/java/org/giefront/DTO/Personne.java
@@ -16,6 +16,22 @@ public class Personne extends Contact implements Serializable {
         this.prenom = prenom;
     }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
     public Personne() {
 
     }
