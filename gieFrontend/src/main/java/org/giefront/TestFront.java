8a0260675ada06eadc961e840dcc904ff676bdfd
ayoubMah <ayoubelmahjouby9@gmail.com>
2024-04-28 23:06:37 +0100

    a commit guys !

--- gieFrontend/src/main/java/org/giefront/TestFront.java
+++ gieFrontend/src/main/java/org/giefront/TestFront.java
@@ -15,11 +15,11 @@ public class TestFront extends Application {
     public void start(Stage stage) throws IOException {
         stage.initStyle(StageStyle.UNDECORATED);
         //FXMLLoader fxmlLoader = new FXMLLoader(TestFront.class.getResource("fournisseur.fxml"));
-        Parent root  = FXMLLoader.load(getClass().getResource("fournisseur.fxml"));
+        Parent root  = FXMLLoader.load(getClass().getResource("homePage.fxml"));
         Scene scene = new Scene(root);
         //Scene scene = new Scene(fxmlLoader.load());
-        stage.setHeight(600);
-        stage.setWidth(900);
+//        stage.setHeight(600);
+//        stage.setWidth(900);
         stage.setResizable(true);
         stage.setTitle("Test");
         stage.setScene(scene);
